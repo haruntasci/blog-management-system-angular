d202c6cbb37f7b6cac74579ef93490632702a5e5
Harun TAŞCI <haruntasci77@gmail.com>
2023-07-16 21:59:23 +0300

    Add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AlertBoxComponent } from './components/alert-box/alert-box.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlertBoxComponent,
     ButtonComponent,
-    ConfirmDialogComponent
+    ConfirmDialogComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
