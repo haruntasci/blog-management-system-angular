785151d6e0d5100d3a1db56a2349ef74a5f392e3
Harun TAŞCI <haruntasci77@gmail.com>
2023-07-16 21:56:46 +0300

    Add alert-box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertBoxComponent } from './components/alert-box/alert-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertBoxComponent
   ],
   imports: [
     BrowserModule,
